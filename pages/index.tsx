33c27324619a59d308805efd032bac69e48a4818
Mickey9878 <goukaku0035@gmail.com>
2022-08-26 04:29:14 +0800

    migrate getProps from index.tsx to fetch.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import React, {Component, useState} from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Button, Text } from '@chakra-ui/react'
-import fetch from "isomorphic-fetch";
+import getProps from '../src/components/api/fetch';
 
+const Home: NextPage=  () => {
 
-const Home: NextPage= ({}:any) => {
   return(
-    <Button onClick={Fetch}>greet</Button>
+    <div>
+      <Button onClick={getProps}>greet</Button>
+    </div>
   );
 }
 
-export async function Fetch() {
-  // try {
-    const url = "http://localhost:4000/address";
-    // const params: any = {
-    //   method: "POST",
-    //   body: "fsjljfklsakla"
-    //   // body: JSON.stringify({address : "123fafa"}),
-    //   // headers: {
-    //   // 'Content-Type': 'application/json'},
-    // };
-
-    const response = await fetch(url, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({ query: "query" }),
-    });
-    const { data } = await response.json();
-    return data;
-    
-    // const response = await fetch(url);
-    // const data = await response.json();
-    // console.log(data);
-    // return { props: { data } };
-  // } catch (error) {
-  //   console.log('error')
-  // }
-}
-
 export default Home;
-
--- src/components/api/fetch.ts
+++ src/components/api/fetch.ts
@@ -1,27 +1,22 @@
-export async function Fetch() {
-    const url = "http://localhost:4000";
-    const params = {method : "POST", body : JSON.stringify({'address' : "123fafa"})};
-    
-    const response = await fetch(url, params);
-    console.log(response)
+import fetch from 'isomorphic-unfetch'
+
+const getProps = async () => {
+    const url = "http://localhost:4000/address";
+  
+    const response = await fetch(url, {
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({query: "query"}),
+    })
     const data = await response.json();
     console.log(data);
-      
-    // 外部のAPIからデータを取得
-    // const addr = 'jfalsjdklajklsakjf'
-    // const res = await fetch(`http://localhost:4000`, {
-    //   method: "POST",
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     address: 'falsjdklajklsakjf'
-    //   })
-    // })
-    // const data = await res.json();
-    // console.log({data});
-    // const greet = data.greet;
-    // const greet='hello'
-    // propsを通してdataを渡す
-    return { props: { data } };
+    return { props: { data } }
+  
 }
+
+export default getProps
